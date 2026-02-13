ea6c7b2ea0dd8e4da7f14e93755a97757a27b4ba
Shrvani21 <255897521+Shrvani21@users.noreply.github.com>
2026-02-13 08:17:19 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/pages/Login.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+function Login({ onLogin }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+
+    try{
+      const response = await fetch("http://localhost:5000/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+      const data = await response.json();
+      if (response.ok) {
+        onLogin();
+      } else {
+        setError(data.message || "Login failed");
+      }
+    } catch (err) {
+      setError("An error occurred during login / Server might be down");
+    }
+  };
+
+    return (
+        <div style={{ maxWidth: "400px", margin: "0 auto", padding: "20px", }}>
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+            <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+
+          <button type="submit">Login</button>
+          </form>
+            {error && <p style={{ color: "red" }}>{error}</p>}
+            </div>
+    );
+}
+export default Login;
---
+++ src/pages/Registration.jsx
@@ -0,0 +1,100 @@
+import { useState } from "react";
+
+function Registration({onRegisterSuccesful}) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+
+    if (!name || !email || !password) {
+      setMessage("All fields are required");
+      return;
+    }
+
+    try {
+      setLoading(true);
+      setMessage("");
+      const res = await fetch("http://localhost:5000/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, email, password }),
+      });
+      const data = await res.json();
+      if (res.ok) {
+        // Successful registration
+        setMessage(data.message || "Registration Successful!");
+        // clear form
+        setName("");
+        setEmail("");
+        setPassword("");
+        setLoading(false);
+        setTimeout(() => {
+          onRegisterSuccesful();
+        }, 2000);
+        return;
+      }
+
+      // Non-OK response (400/500) -> show server-provided message if any
+      setMessage(data.message || "Registration failed");
+      setLoading(false);
+  } catch (err) {
+    console.error(err);
+    setMessage("Server error");
+    setLoading(false);
+  }
+  };
+
+
+
+
+  return (
+    <div style={{ width: "300px", margin: "50px auto" }}>
+      <h2>Register</h2>
+
+      {message && <p>{message}</p>}
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            placeholder="Enter name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            autoComplete="name"
+          />
+        </div>
+
+        <div>
+          <input
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            autoComplete="email"
+          />
+        </div>
+
+        <div>
+          <input
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            autoComplete="new-password"
+          />
+        </div>
+
+        <button type="submit" disabled={loading}>
+          {loading ? "Registering..." : "Register"}</button>
+      </form>
+    </div>
+  );
+}
+
+export default Registration;
